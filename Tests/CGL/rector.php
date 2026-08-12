90b93e37b46344cdb5b31e0bdfda4e81bfcd45ce
Konrad Michalik <km@move-elevator.de>
2026-08-12 16:51:43 +0200

    style: apply CGL formatting to Task 1 tooling files

--- Tests/CGL/rector.php
+++ Tests/CGL/rector.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the "typo3_mcp_server_content_planner" TYPO3 CMS extension.
+ *
+ * (c) 2026 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Rector\Config\RectorConfig;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\PostRector\Rector\NameImportingPostRector;
